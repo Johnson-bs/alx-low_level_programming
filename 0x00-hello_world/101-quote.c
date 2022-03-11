4dce68cfdc7e40ded4e58791221668a0346300fc
Johnson-bs <johnteemyself0000@gmail.com>
2022-03-11 06:52:39 -0800

    My quote.c coommit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * My print quote task
+ * Return: return 1
+ */
+int main(void)
+{
+ptintf("and that piece of art is useful" - Dora Kopar, 2015-10-19\n");
+return (1);
+}
