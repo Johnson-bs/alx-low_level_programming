4d6c54cfd61213f22125d3dee1b042f9df908d5f
Johnson-bs <johnteemyself0000@gmail.com>
2022-03-22 13:32:51 -0700

    My strlen commit

---
+++ 0x05-pointers_arrays_strings/strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - length of a string
+ * @s: A pointer to an int that will be changed or updated
+ * Return: void to mean our answer is correct
+ */
+
+int _strlen(char *s)
+{
+	int u;
+
+	u = 0;
+	while (s[u] != '\0')
+	{
+		u++;
+	}
+	return (u);
+}
