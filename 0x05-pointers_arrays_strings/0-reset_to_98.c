9f45f472e4b36ebd2c81cd1d9b2ded61e1dcc07a
Johnson-bs <johnteemyself0000@gmail.com>
2022-03-22 13:18:38 -0700

    My reset to 98 commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,12 @@
 #include "main.h"
+
 /**
  * reset_to_98 - updates an integer value to 98
  * @n: A ponter to an int that will be updated
+ *
  * Return: void that means is correct
  */
+
 void reset_to_98(int *n)
 
 {
