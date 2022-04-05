4d84e14d7a1743452721a062889835c5c969077e
Johnson-bs <johnteemyself0000@gmail.com>
2022-04-05 13:23:52 -0700

    My factorial commit

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * factorial - factorial of a given number
- * @s: pointer block memory to fill
+ * @n: pointer block memory to fill
  * Return: factorial
  */
 
@@ -10,7 +10,7 @@ int factorial(int n)
 	if (n == 0) /*Base condition*/
 		return (1);
 
-	else id (n < 0) /*Base condition*/
+	else if (n < 0) /*Base condition*/
 		return (-1);
 	else
 		return (n * factorial(n - 1)); /*Recursive call*/
