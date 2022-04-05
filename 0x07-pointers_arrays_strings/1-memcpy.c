333fd7c678f1e0f814f8f80452712834f51fdf68
Johnson-bs <johnteemyself0000@gmail.com>
2022-04-05 07:19:58 -0700

    Memcpy commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,22 @@
 #include "main.h"
 /**
- * _memset - copy number bytes from address "from" to address "to"
+ * _memcpy - copy number bytes from address "from" to address "to"
  * @dest: content is to be copied
  * @src: Source of data to be copied
+ * @n: bytes of the memory
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n) /*Decalring WHILE*/
+	{
+		*(dest + i) = *(src + i); /*add 1 position dest and src*/
+		i++;
+	} /*END WHILE*/
+
+	return (dest);
+}
