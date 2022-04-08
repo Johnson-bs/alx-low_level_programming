a2163b75e04c2330956b71854caa1f064ff132d8
Johnson-bs <johnteemyself0000@gmail.com>
2022-04-08 07:45:58 -0700

    My mul commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,20 +1,22 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
  * main - Prints the name of the program
  * @argc: count argument
  * @argv: value
- * Return: always 0 (success), 1 (false)
+ * Return: 0 if true, 1 if false
  */
 int main(int argc, char *argv[])
 {
 	int a, b;
+
 	if (argc == 3)
 	{
 		a = atoi(argv[1]);
 		b = atoi(argv[2]);
-		printf("%d\n", a *b);
+		printf("%d\n", a * b);
 		return (0);
 	}
 	printf("Error\n");
